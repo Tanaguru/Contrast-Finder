a5808ae9c2550d21cb160107988b61c00583dc8a
amezigh <aallioui@oceaneconsulting.com>
2017-01-06 13:16:30 +0100

    Remove sticky "old colors" banner (affix.js)

--- contrast-finder-webapp/src/main/webapp/Js/affix.js
+++ contrast-finder-webapp/src/main/webapp/Js/affix.js
@@ -1,10 +1,10 @@
-$('#old-contrast').affix(
-        {
-            offset: {
-                top: function() {
-                    var header = $('#header').height()
-                    var setUpForm = $('#set-up-form').height()
-                    return (this.top = header + setUpForm + 135)
-                }
+/*$('#old-contrast').affix(
+    {
+        offset: {
+            top: function() {
+                var header = $('#header').height()
+                var setUpForm = $('#set-up-form').height()
+                return (this.top = header + setUpForm + 135)
             }
-        })
+        }
+    })*/
