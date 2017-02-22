ddc67668df6607baebd45bee9d13a67e9f982300
amezigh <aallioui@oceaneconsulting.com>
2017-02-22 09:43:09 +0100

    Update css/js in webapp: fixed table "results" header with "old contrast" table

--- contrast-finder-webapp/src/main/webapp/Js/affix.js
+++ contrast-finder-webapp/src/main/webapp/Js/affix.js
@@ -1,10 +1,27 @@
-$('#old-contrast').affix(
-    {
-        offset: {
-            top: function() {
-                var header = $('#header').height()
-                var setUpForm = $('#set-up-form').height()
-                return (this.top = header + setUpForm + 68)
-            }
-        }
-    })
+$(function() {	
+	$('#old-contrast, #contrast-solution thead').affix({
+		offset: {
+		    top: function() {
+				var header = $('#header').height(),
+				    setUpForm = $('#set-up-form').height(),
+				    limit = header + setUpForm + 68;
+				return (this.top = header + setUpForm + 68)
+		    }
+		}
+	});
+
+	$(window).on('resize scroll', function() {
+		var header = $('#header').height(),
+		    setUpForm = $('#set-up-form').height(),
+		    limit = header + setUpForm + 68;
+
+		if ( 
+			$(this).scrollTop() > limit && 
+			$(this).width() >= 768
+		) {
+		    $('body').css('margin-top', '160px');
+		} else {
+		   $('body').css('margin-top', 'inherit');
+		}
+	});
+});
