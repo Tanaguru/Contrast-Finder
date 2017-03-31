1708912ec13b2d6f0e0f62ef71db0cf5522142e6
amezigh <aallioui@oceaneconsulting.com>
2017-03-31 16:09:03 +0200

    Results sorted by distance by default + value of the sorting in the URL

--- contrast-finder-webapp/src/main/webapp/Js/accessible.js
+++ contrast-finder-webapp/src/main/webapp/Js/accessible.js
@@ -1,4 +1,4 @@
-    $(document).ready(function() {
+$(document).ready(function() {
 	var gTableCaptionText = $("#contrast-solution caption").text();
 
 	$.tablesorter.addWidget({
@@ -48,9 +48,25 @@
 		}
 	});
 
+	// Check if the sorting by distance is defined in the URL
+	var sort = 0,
+			urlParams = window.location.search.substring(1);
+
+	if ( urlParams.indexOf('distance') > -1 && urlParams.split('distance=')[1] == 'desc') sort = 1;
+	
+	if ( urlParams.indexOf('distance') > -1)
+		urlParams = urlParams.substring(0, urlParams.indexOf('&distance='));
+	
+	// update URL with the parameter "distance" when page is loaded
+	var distanceSort = sort == 0 ? 'asc' : 'desc';
+	history.pushState('', '', '?' + urlParams + '&distance=' + distanceSort); 	
+
 	// make the good table
 	$("#contrast-solution")
-		.tablesorter({widgets: ['addA11y']})
+		.tablesorter({
+			widgets: ['addA11y'],
+			sortList: [[4, sort]]
+		})
 		.wrap('<div role="application">') // wrap it in a DIV with a role of application
 		.attr("role", "grid") // add role of grid to the table itself
 		.attr("aria-readonly", "true")  // mark the grid as readonly (because none of the data is editable
@@ -92,5 +108,21 @@
 		'aria-live': 'assertive',
 		'aria-relevant': 'all',
 		'aria-atomic': 'true'
+	});
+
+
+  // Update the parameter "distance" in the URL 
+  // when man click the "sort by distance" button
+	$('#contrast-solution .col05 div').on('click', function() {
+		// Get value of the parameter or set one
+		var urlParams = window.location.search.substring(1),
+				currentSort = urlParams.indexOf('distance') > -1 ? (urlParams.indexOf('asc') > -1 ? 'desc' : 'asc') : 'desc';
+
+		// If url contains the string "&distance=[...]", we delete the string in order to update the url afterwards 
+		if ( urlParams.indexOf('distance') > -1 )
+			urlParams = urlParams.substring(0, urlParams.indexOf('&distance='));
+
+		// Update the url with the good "distance" param
+		history.pushState('', '', '?' + urlParams + '&distance=' + currentSort); 	
 	});
 });
